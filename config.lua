8389ed6ebeb037e2380ee8873965ac61c0d7ded3
rbarra <82441559+FlawetDev@users.noreply.github.com>
2024-02-26 23:56:41 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,8 +2,6 @@ Config = {}
 
 Config = {
     ------------
-    ESXTrigger =  exports["es_extended"]:getSharedObject(),
-    ------------
     ColorMenuR = 255, -- Bannière couleur R
     ColorMenuG = 11, -- Bannière couleur G
     ColorMenuB = 11, -- Bannière couleur B
